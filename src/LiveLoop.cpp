387fc82d9c4caa8f0b2c2a7bbdd207f11642efe2
Sebastian Damrath <sebastian.damrath@gmail.com>
2019-01-11 15:42:26 +0100

    Clean up comments and use const ref

--- src/LiveLoop.cpp
+++ src/LiveLoop.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "../includes/stdafx.h"
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <memory>
@@ -11,14 +11,14 @@ const int ROI_WIDTH = 100;
 const int ROI_HEIGHT = 100;
 
 cv::Rect ROI_RECT(cv::Point2d((MY_IMAGE_WIDTH / 2.0) - (ROI_WIDTH / 2.0), (MY_IMAGE_HEIGHT / 2.0) - (ROI_HEIGHT / 2.0)),
-						cv::Point2d((MY_IMAGE_WIDTH / 2.0) + (ROI_WIDTH / 2.0), (MY_IMAGE_HEIGHT / 2.0) + (ROI_HEIGHT / 2.0)));
+						cv::Point2d((MY_IMAGE_WIDTH / 2.0) + (ROI_WIDTH / 2.0),
+										(MY_IMAGE_HEIGHT / 2.0) + (ROI_HEIGHT / 2.0)));
 
 cv::Mat ROI_HIST;
 
-cv::Mat process(cv::Mat& inputFrame);
+cv::Mat process(cv::Mat &inputFrame);
 
-int MonoLoop()
-{
+int MonoLoop() {
 	cv::VideoCapture cap(0);
 
 	if (!cap.isOpened())
@@ -51,7 +51,7 @@ int MonoLoop()
 
 
 	// First loop to capture the region of interest
-	while(true)
+	while (true)
 	{
 
 		if (!cap.read(inputFrame))
@@ -60,36 +60,31 @@ int MonoLoop()
 			break;
 		}
 
-		/*******************************todo*****************************/
 		outputFrame = inputFrame;
 
-		// Draw target square
+		// Rectangle to indicate Region Of Interest
 		cv::rectangle(outputFrame, ROI_RECT, cv::Scalar(0, 255, 0));
-		/***************************end todo*****************************/
 
 		imshow("cam", outputFrame);
 
+		// Use waitkey so user can control when ROI is captured
 		if (cv::waitKey(MY_WAIT_IN_MS) == 27)
 		{
 			outputFrame(ROI_RECT).copyTo(roi);
 			cv::cvtColor(roi, hsv_roi, cv::COLOR_BGR2HSV);
 			int hbins = 30;
 			int sbins = 32;
-			int histSize[] = { hbins, sbins };
+			int histSize[] = {hbins, sbins};
 
 			float hranges[] = {0, 180};
 			float sranges[] = {0, 256};
-			const float* ranges[] = { hranges, sranges };
-			int channels[] = { 0, 1 };
+			const float *ranges[] = {hranges, sranges};
+			int channels[] = {0, 1};
 			cv::calcHist(&hsv_roi, 1, channels, cv::Mat(), ROI_HIST, 2, histSize, ranges);
 			imshow("debug", hsv_roi);
 			break;
 		}
 	}
-
-//	cv::Mat roi;
-//	cv::Mat hsv_roi = cv::cvtColor(roi, cv::COLOR_BGR2HSV);
-//	cv::calcHist();
 
 	while (true)
 	{
@@ -99,55 +94,51 @@ int MonoLoop()
 			break;
 		}
 
-		/*******************************todo*****************************/
 		outputFrame = process(inputFrame);
-		/***************************end todo*****************************/
-
-		//imshow("roi", ROI_HIST);
 		imshow("cam", outputFrame);
+
 
 		if (cv::waitKey(MY_WAIT_IN_MS) == 27)
 		{
-			std::cout << "ESC key is pressed 	cv::Mat reference_image;by user" << std::endl;
+			std::cout << "ESC key is pressed" << std::endl;
 			break;
 		}
 	}
 	return 0;
 }
 
-cv::Mat process(cv::Mat& inputFrame)
-{
-    auto outputFrame = inputFrame;
+cv::Mat process(cv::Mat &inputFrame) {
+	auto outputFrame = inputFrame;
 	cv::Mat dilation_kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(4, 4));
 	cv::Mat erosion_kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(8, 8));
 
-    cv::Mat hsv, backproject;
-   	cv::cvtColor(inputFrame, hsv, cv::COLOR_BGR2HSV);
-   	int channels[] = { 0, 1 };
+	cv::Mat hsv, backproject;
+	cv::cvtColor(inputFrame, hsv, cv::COLOR_BGR2HSV);
+	int channels[] = {0, 1};
 	float hranges[] = {0, 180};
 	float sranges[] = {0, 256};
-	const float* ranges[] = { hranges, sranges };
-   	cv::calcBackProject(&hsv, 1, channels, ROI_HIST, backproject, ranges);
+	const float *ranges[] = {hranges, sranges};
+	cv::calcBackProject(&hsv, 1, channels, ROI_HIST, backproject, ranges);
 
 	cv::GaussianBlur(backproject, backproject, cv::Size(5, 5), 1);
 	cv::erode(backproject, backproject, erosion_kernel);
 	cv::dilate(backproject, backproject, dilation_kernel);
-   	cv::RotatedRect tracking_data = cv::CamShift(backproject, ROI_RECT, cv::TermCriteria());
-   	std::vector<cv::Point2f> points(4);
+	cv::RotatedRect tracking_data = cv::CamShift(backproject, ROI_RECT, cv::TermCriteria());
+	std::vector<cv::Point2f> points(4);
 
-   	//cv::boxPoints(tracking_data, points);
-   	tracking_data.points(points.data());
+	//cv::boxPoints(tracking_data, points);
+	tracking_data.points(points.data());
 
-   	std::vector<cv::Point> poly_points;
-   	for (auto v : points)
+	std::vector<cv::Point> poly_points;
+	for (const auto &v : points)
 	{
-   		poly_points.push_back(v);
+		poly_points.push_back(v);
 	}
-   	cv::polylines(outputFrame, poly_points, true, cv::Scalar(0, 255, 0));
+	cv::polylines(outputFrame, poly_points, true, cv::Scalar(0, 255, 0));
 
-   	imshow("roi", backproject);
+	imshow("roi", backproject);
 
-    return outputFrame;
+	return outputFrame;
 }
 
 int main(int argc, char *argv[]) {
